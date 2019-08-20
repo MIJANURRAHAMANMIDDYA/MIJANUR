5062be706b35481d38efb002e6afa99013c2351b
MIJANURRAHAMANMIDDYA <mijanurrahamanmiddya111@gmail.com>
2019-08-20 18:20:07 +0530

    power of two

---
+++ Mijanur Rahaman/felloshipProgram/src/org/Bridgelabzsolution/funtionalprogram/Poweroftwo.java
@@ -0,0 +1,15 @@
+package org.Bridgelabzsolution.funtionalprogram;
+
+public class Poweroftwo {
+
+	public static void main(String[] args) {
+		int N = 31;
+		System.out.println(" table contain power of two:");
+		for (int i = 0; i <= N; i++) {
+			double value = Math.pow(2, i);
+			System.out.println(+value);
+		}
+
+	}
+
+}
