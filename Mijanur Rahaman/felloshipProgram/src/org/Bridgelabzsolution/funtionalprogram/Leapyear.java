7ef77d532e63371c3cc4f688a6fe97e1463598a8
MIJANURRAHAMANMIDDYA <mijanurrahamanmiddya111@gmail.com>
2019-08-20 18:00:19 +0530

    updated

---
+++ Mijanur Rahaman/felloshipProgram/src/org/Bridgelabzsolution/funtionalprogram/Leapyear.java
@@ -0,0 +1,37 @@
+package org.Bridgelabzsolution.funtionalprogram;
+
+import java.util.Scanner;
+
+public class Leapyear {
+
+	public static void main(String[] args)
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter year:");
+		int year=sc.nextInt();
+		if(year<1000 || year>9999)
+		{
+			System.out.println("not acceptable");
+		
+		}
+		if(year%4==0)
+		{
+			if(year%100==0)
+			{
+				if(year%400==0)
+				{
+					System.out.println(year+" year is a leapyear");
+				}
+				else
+					System.out.println(year+" year is a leapyear");
+			}
+		}
+			else
+				System.out.println(year+" year is  not  a leapyear");
+		}
+	
+		
+	}
+
+
+
