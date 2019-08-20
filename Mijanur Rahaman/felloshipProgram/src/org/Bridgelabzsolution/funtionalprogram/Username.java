cebc0065a031c0f080cdbf9efb5241ff8ec8a0f0
MIJANURRAHAMANMIDDYA <mijanurrahamanmiddya111@gmail.com>
2019-08-20 12:05:05 +0530

    username

---
+++ Mijanur Rahaman/felloshipProgram/src/org/Bridgelabzsolution/funtionalprogram/Username.java
@@ -0,0 +1,16 @@
+package org.Bridgelabzsolution.funtionalprogram;
+
+import java.util.Scanner;
+
+public class Username {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your name:");
+		String userName=sc.next();
+		System.out.println("Hello " +userName+", How are you?");
+		
+	}
+
+}
