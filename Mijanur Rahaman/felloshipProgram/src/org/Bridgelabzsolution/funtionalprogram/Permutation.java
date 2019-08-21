a7b40c6ea997b906ed4809eed0b395b1fd5dbef6
MIJANURRAHAMANMIDDYA <mijanurrahamanmiddya111@gmail.com>
2019-08-21 17:30:24 +0530

    updated on 21/08/2019

--- Mijanur Rahaman/felloshipProgram/src/org/Bridgelabzsolution/funtionalprogram/Permutation.java
+++ Mijanur Rahaman/felloshipProgram/src/org/Bridgelabzsolution/funtionalprogram/Permutation.java
@@ -1,9 +1,43 @@
 package org.Bridgelabzsolution.funtionalprogram;
 
+import java.util.Scanner;
+
 public class Permutation {
 
-	public static void main(String[] args) {
-		Scanner
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the string:");
+		String s = sc.next();
+		int n = s.length();
+		Permutation permutation = new Permutation();
+		permutation.permute(s, 0, n - 1);
+	}
+
+private String permute(String s, int l, int r) 
+{ 
+    if (l == r) 
+        System.out.println(s); 
+    else
+    { 
+        for (int i = l; i <= r; i++) 
+        { 
+            s = swap(s,l,i); 
+            permute(s, l+1, r); 
+            s = swap(s,l,i); 
+        }
+    }
+	return s;
+}
+	String swap(String a, int i, int j) 
+	{
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
 	}
 
 }
+
